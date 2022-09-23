6c7b23983b7bde732db7e734ec571727ec8cef69
Hossein Pourdavar <h.pourdavar@gmail.com>
2022-09-23 22:32:20 +0330

    update app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 import { RepositoryService } from './common/repository/services/repository.service';
+
+class UserDTO {
+  constructor(public readonly name: string) {}
+}
 
 @Injectable()
 export class AppService {
   constructor(private readonly repository: RepositoryService) {}
 
-  async findUserById({ name }: any) {
+  async findUserById({ name }: UserDTO) {
     const user = await this.repository.users.findOne({ name });
+
+    if (!user) throw new BadRequestException('USER:NOT_FOUND');
+
+    return user;
   }
 }
